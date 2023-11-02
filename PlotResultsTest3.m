862989c8ba7e8538e043d25d76923e25aaa6aed1
aidanstapleton <89906505+aidanstapleton@users.noreply.github.com>
2023-11-02 14:42:03 +1100

    Final Commit

--- PlotResultsTest3.m
+++ PlotResultsTest3.m
@@ -1,8 +1,15 @@
-% AMME4112 - Thesis B
+%
+% IMU-Based 3D Human Pose Tracking System
 %
 % Author: Aidan Stapleton
-% Date: 012/09/2023
 %
+% Date: 03/10/2023
+%
+% Experiment 3: Elevation of the right leg in front of 
+% the body to a 30 degree angle of depression and back
+% again. The Y and Z components of the right leg 
+% position is tracked and compared with stereo vision 
+% ground truth data.
 
 % Clear the workspace and command window
 cla;
@@ -10,23 +17,12 @@ clc;
 clear;
 close all;
 
-% Validate data from the right leg moving from the initial position to
-% infront of their body at 30 degrees to the horizontal and back.
-
 % Read the data as floating-point numbers
-KneeData = load('Test2ResultsKnee4.txt');
-AnkleData = load('Test2ResultsAnkle4.txt');
+KneeData = load('Test3KneeData.txt');
+AnkleData = load('Test3AnkleData.txt');
 
 % Define x axis data
 T = linspace(1, length(KneeData), length(KneeData));
-
-% Define the ground truth for moving the wrist from point 1 to
-% point 8 and then back down to point 1
-KneeGroundTruthY = [0, 0.4107];
-KneeGroundTruthZ = [-0.7763, -0.5897];
-
-AnkleGroundTruthY = [0, 0.7467];
-AnkleGroundTruthZ = [-1.1923, -0.7673];
 
 % Get the Y and Z data
 KneeDataY = KneeData(:, 2);
@@ -49,13 +45,8 @@ Ry = [cos(thetaY) 0 sin(thetaY); 0 1 0; -sin(thetaY) 0 cos(thetaY)];
 thetaZ = 0*(pi/180);
 Rz = [cos(thetaZ) -sin(thetaZ) 0; sin(thetaZ) cos(thetaZ) 0; 0 0 1];
 
-% Calculate the errors for X and Z
-%ErrorX = MatchedY - GroundTruthY;
-% Xc1_Knee = [0.3655, 0.4895, 3.1336];
-% Xc2_Knee = [0.0678, 0.2210, 2.9218];
-% Xc1_Ankle = [0.3396, 0.8570, 3.5043];
-% Xc2_Ankle = [-0.2951, 0.3959, 3.2183];
-
+% Define the stereo vision knee and ankle positions 
+% in the camera coordinate frame 
 Xc1_Knee = [0.8125, 0.4685, 3.1336];
 Xc2_Knee = [0.4678, 0.2110, 2.9218];
 Xc1_Ankle = [0.7896, 0.8570, 3.5043];
@@ -63,7 +54,6 @@ Xc2_Ankle = [0.0951, 0.3959, 3.2183];
 
 % Rotate and invert camera axes to roughly align with the world
 % coordinate frame.
-
 % Apply a thetaX degree rotation about x
 Xc1_Knee = Xc1_Knee*Rx;
 Xc2_Knee = Xc2_Knee*Rx;
@@ -119,9 +109,9 @@ KneeZ2SubSampled = [-0.5009, -0.4923, -0.5028];
 AnkleZ1SubSampled = [-1.1940, -1.1890, -1.1880];
 AnkleZ2SubSampled = [-0.7390, -0.7179, -0.7435];
 
-% -------------------------------------- Error ---------------
+% -------------------------------------- Error ----------------------------
 
-% Define the erorr subsamples
+% Define the error for each subsubsample
 KneeY1Error = abs(Xw1_Knee(1) - KneeY1SubSampled);
 KneeY2Error = abs(Xw2_Knee(1) - KneeY2SubSampled);
 AnkleY1Error = abs(Xw1_Ankle(1) - AnkleY1SubSampled);
@@ -132,25 +122,25 @@ KneeZ2Error = abs(Xw2_Knee(3) - KneeZ2SubSampled);
 AnkleZ1Error = abs(Xw1_Ankle(3) - AnkleZ1SubSampled);
 AnkleZ2Error = abs(Xw2_Ankle(3) - AnkleZ2SubSampled);
 
-% Define the average error for each point in cm
+% Define the average error for each point in the Y axis in cm
 AvgKneeY1Error = (sum(KneeY1Error)/length(KneeY1Error))*10^2;
 AvgKneeY2Error = (sum(KneeY2Error)/length(KneeY2Error))*10^2;
 AvgAnkleY1Error = (sum(AnkleY1Error)/length(AnkleY1Error))*10^2;
 AvgAnkleY2Error = (sum(AnkleY2Error)/length(AnkleY2Error))*10^2;
 
+% Define the average errors in the Y axis for each joint
 AvgKneeYError = (AvgKneeY1Error + AvgKneeY2Error)/2;
 AvgAnkleYError = (AvgAnkleY1Error + AvgAnkleY2Error)/2;
 
+% Define the average error for each point in the Z axis in cm
 AvgKneeZ1Error = (sum(KneeZ1Error)/length(KneeZ1Error))*10^2;
 AvgKneeZ2Error = (sum(KneeZ2Error)/length(KneeZ2Error))*10^2;
 AvgAnkleZ1Error = (sum(AnkleZ1Error)/length(AnkleZ1Error))*10^2;
 AvgAnkleZ2Error = (sum(AnkleZ2Error)/length(AnkleZ2Error))*10^2;
 
+% Define the average errors in the Z axis for each joint
 AvgKneeZError = (AvgKneeZ1Error + AvgKneeZ2Error)/2;
 AvgAnkleZError = (AvgAnkleZ1Error + AvgAnkleZ2Error)/2;
-
-KneeColour = 'b';
-AnkleColour = 'r';
 
 % Define colours
 str = '#A8142F';
@@ -169,10 +159,8 @@ grid on;
 xlim([0 400]);
 ylim([-0.1 0.9]);
 
-% yline(KneeGroundTruthY(1), KneeColour);
-% yline(KneeGroundTruthY(2), KneeColour);
-% yline(AnkleGroundTruthY(1), AnkleColour);
-% yline(AnkleGroundTruthY(2), AnkleColour);
+% Plot the stereo vision data in the Z axis as horizontals for each point
+% of interest
 
 % Invert the x value as we are measuring y in the x
 % direction along the wall
@@ -186,6 +174,7 @@ xlabel('Data Sample');
 ylabel('Right Leg Position Y (m)');
 legend('Right Knee Position Y', 'Right Ankle Position Y');
 
+% Plot Z data
 figure;
 hold on;
 plot(T(1:350), KneeDataZ(1:350), '-o', 'Color', blue);
@@ -194,11 +183,8 @@ grid on;
 xlim([0 400]);
 ylim([-1.3 -0.3]);
 
-% yline(KneeGroundTruthZ(1), KneeColour);
-% yline(KneeGroundTruthZ(2), KneeColour);
-% yline(AnkleGroundTruthZ(1), AnkleColour);
-% yline(AnkleGroundTruthZ(2), AnkleColour);
-
+% Plot the stereo vision data in the Z axis as horizontals for each point
+% of interest
 yline(Xw1_Knee(3), 'Color', blue, 'LABEL', 'Knee A', 'LabelHorizontalAlignment', 'right');
 yline(Xw2_Knee(3), 'Color', blue, 'LABEL', 'Knee B', 'LabelHorizontalAlignment', 'right');
 yline(Xw1_Ankle(3), 'Color', red, 'LABEL', 'Ankle A', 'LabelHorizontalAlignment', 'right');
